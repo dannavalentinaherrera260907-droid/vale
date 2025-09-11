53f23659cf605530340840df6c02f656bde1d946
Vale <dannavalentinaherrera260907@gmail.com>
2025-09-11 15:57:01 -0500

    Wow

---
+++ prac.cpp
@@ -0,0 +1,11 @@
+int main(){
+    if(num1)cout<<"El número es identificado como true";
+
+    int arr[5];
+    for(int i=0;i<5;i++){
+        arr[i]=pow(2;i);
+        cout<<"arr["<<i<<"]: "<<arr[i]<<end1;
+    }
+    int matr[3][3];
+    matr[0][0]=0;
+}
