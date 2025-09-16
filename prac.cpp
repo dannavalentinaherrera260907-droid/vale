c612e61cfc11eba5168c6769e91372e962c9ce03
Vale <dannavalentinaherrera260907@gmail.com>
2025-09-15 20:55:07 -0500

    ejemplo2.cpp

--- prac.cpp
+++ prac.cpp
@@ -1,11 +1,14 @@
-int main(){
-    if(num1)cout<<"El número es identificado como true";
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-    int arr[5];
-    for(int i=0;i<5;i++){
-        arr[i]=pow(2;i);
-        cout<<"arr["<<i<<"]: "<<arr[i]<<end1;
+int main(){
+        int arr[5];
+    for(int i = 0 ; i < 5 ; i++) {
+        arr[i] = static_cast<int>(pow(2.0, i));
+        std::cout << "arr["<<i<<"]: " << arr[i] << std::endl;
     }
     int matr[3][3];
     matr[0][0]=0;
+
 }
