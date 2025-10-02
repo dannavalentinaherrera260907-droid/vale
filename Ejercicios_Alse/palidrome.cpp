9fccaba06f69d1aa07ba345440be0708dbd0abb5
Vale <dannavalentinaherrera260907@gmail.com>
2025-10-01 23:45:02 -0500

    palidromenumber.cpp

---
+++ Ejercicios_Alse/palidrome.cpp
@@ -0,0 +1,20 @@
+//Danna Valentina Herrrera Ortiz y Juan Esteban Guamán Lancheros
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if (x < 0) return false;
+
+        long reversed = 0;
+        int original = x;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+};
