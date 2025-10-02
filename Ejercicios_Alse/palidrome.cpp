622a05e92af4f0fab457698a420090e04a7804b5
Vale <dannavalentinaherrera260907@gmail.com>
2025-10-01 23:53:28 -0500

    actualizado  palidromenumber.cpp

--- Ejercicios_Alse/palidrome.cpp
+++ Ejercicios_Alse/palidrome.cpp
@@ -1,9 +1,11 @@
 //Danna Valentina Herrrera Ortiz y Juan Esteban Guamán Lancheros
+#include <iostream>
+using namespace std;
 
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
+       
         if (x < 0) return false;
 
         long reversed = 0;
@@ -18,3 +20,18 @@ public:
         return original == reversed;
     }
 };
+
+int main() {
+    Solution sol;
+    int x;
+
+    cout << "Ingrese un número: ";
+    cin >> x;
+
+    if (sol.isPalindrome(x))
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
+
+    return 0;
+}
